ea0e6a4e25c0836343ee955d70be8eb2f76b1153
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2022-11-09 19:35:09 +0100

    front removed, back filled in

--- borrowbooksapp/app.py
+++ borrowbooksapp/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, request
+from flask_pymongo import PyMongo
+import json
+import controllers.BookController as book_controller
 
 
 app = Flask(__name__)
@@ -9,11 +12,60 @@ mongodb_client = PyMongo(app)
 mongo = mongodb_client.db
 
 
+@app.route('/get_books', methods=['GET'])
+def get_books():
+    """Method to obtain a list of all books"""
+    if request.method == "GET":
+        books_list = book_controller.get_books(mongo)
+        return {"message": 200, "books": books_list}
+    return {"message": 500}
 
-@app.route('/api', methods=['GET'])
-def index():
-    return {
-        "tutorial": "Flask React Heroku"
-    }
+
+@app.route('/get_book', methods=['GET'])
+def get_book():
+    """Method to obtain a book"""
+    if request.method == "GET":
+        book = book_controller.get_book(request.data, mongo)
+        return {"message": 200, "book": book}
+    return {"message": 500}
+
+
+@app.route('/create_book', methods=['POST'])
+def create_book():
+    """Method to create a new book"""
+    if request.method == "POST":
+        book_controller.create_book(request.data, mongo)
+        return {"message": 200}
+
+    return {"message": 500}
+
+
+@app.route('/delete_book', methods=['DELETE'])
+def delete_book():
+    """Method to delete a book"""
+    if request.method == 'DELETE':
+        book_controller.delete_book(request.data, mongo)
+        return {"message": 200}
+    return {"message": 500}
+
+
+@app.route('/update_book', methods=['PUT'])
+def update_book():
+    """Method to update a book"""
+    if request.method == 'PUT':
+        book_controller.update_book(request.data, mongo)
+        return {"message": 200}
+    return {"message": 500}
+
+
+@app.route('/search', methods=['GET'])
+def search_books():
+    """Method to search books by parameters"""
+    if request.method == 'GET':
+        books_list = book_controller.search_by(request.data, mongo)
+        return {"message": 200, "book_list": books_list}
+    return {"message": 500}
+
+
 if __name__ == '__main__':
     app.run()
--- borrowbooksapp/controllers/BookController.py
+++ borrowbooksapp/controllers/BookController.py
@@ -1,9 +1,75 @@
-def create_book():
+import json
 
-def update_book():
+from model.Book import Book
+from dao.BookDAO import BookDAO
 
-def get_books():
+book_dao = BookDAO()
 
-def get_book():
+def create_book(data, mongo):
+    """Method to create a new book"""
+    isbn = json.loads(data.decode())["isbn"]
+    title = json.loads(data.decode())["title"]
+    author = json.loads(data.decode())["author"]
+    publisher = json.loads(data.decode())["publisher"]
 
-def delete_book():
+    # Exceptions
+
+    book = Book(isbn, title, author, publisher)
+    book_dao.save(mongo, book.get_json())
+
+
+def delete_book(data, mongo):
+    """Method to delete an existing book"""
+    isbn = json.loads(data.decode())["isbn"]
+
+    #Exceptions
+
+    book_dao.delete_by_isbn(mongo, isbn)
+
+
+def update_book(data, mongo):
+    """Method to update an existing book"""
+    book_json = json.loads(data.decode())
+
+    isbn = book_json["isbn"]
+    title = book_json["title"]
+    author = book_json["author"]
+    publisher = book_json["publiser"]
+
+    book = book_dao.get_by_isbn(mongo, isbn)
+
+    #Exceptions
+
+    book["title"] = title
+    book["author"] = author
+    book["publisher"] = publisher
+
+    book_dao.update(mongo, book)
+
+
+def get_books(mongo):
+    """Method to obtains the books list"""
+    book_list = book_dao.get_all(mongo)
+    return book_list
+
+
+def get_book(data, mongo):
+    """Method to obtain a book"""
+    isbn = json.loads(data.decode())["isbn"]
+    book = book_dao.get_by_isbn(mongo, isbn)
+    return book
+
+
+def search_by(data, mongo):
+    search_type = json.loads(data.decode())["type"]
+    search_data = json.loads(data.decode())["data"]
+
+    if search_type == "title":
+        return_list = book_dao.get_by_title(mongo, search_data)
+    elif search_type == "author":
+        return_list = book_dao.get_by_author(mongo, search_data)
+    else:
+        return_list = book_dao.get_by_publisher(mongo, search_data)
+
+
+
